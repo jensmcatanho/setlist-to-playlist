55da08582cf8a8730a71d14e02476c6da28e443e
Jean Catanho <jensmcatanho@gmail.com>
2019-03-08 15:55:02 -0300

    Replace fetch by axios

--- src/express.js
+++ src/express.js
@@ -1,27 +1,30 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const fetch = require("node-fetch");
-var request = require('request');
+const request = require('request');
+const axios = require('axios');
 const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/artist/:artistName/date/:date', cors(), function(req, res) {
-    fetch(`https://api.setlist.fm/rest/1.0/search/setlists/?artistName=${req.params.artistName}&date=${req.params.date}`, {
-        method: 'GET',
+    let config = {
         headers: {
             "Accept": "application/json",
             "x-api-key": "eb4ad752-21d6-433a-ab6c-0a52a62d7f57"
         }
-    }).then(
-        response => response.json()
-    ).then(
-        data => res.send(data)
+    }
+
+    axios.get(`https://api.setlist.fm/rest/1.0/search/setlists/?artistName=${req.params.artistName}&date=${req.params.date}`, config).then(
+        response => {
+            res.send(response.data)
+        }
     );
 });
 
 app.get('/code/:code', cors(), function(req, res) {
+    // Refactor: change it to use axios
     let authOptions = {
         url: 'https://accounts.spotify.com/api/token',
         form: {
@@ -41,15 +44,17 @@ app.get('/code/:code', cors(), function(req, res) {
 });
 
 app.get('/access_token/:access_token', cors(), function(req, res) {
-    let authOptions = {
-        url: 'https://api.spotify.com/v1/me',
+    let config = {
         headers: {
             'Authorization': 'Bearer ' + req.params.access_token
         }
     }
-    request.get(authOptions, function(error, response, body) {
-        res.send(body);
-    })
+
+    axios.get('https://api.spotify.com/v1/me', config).then(
+        response => {
+            res.send(response.data)
+        }
+    );
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
